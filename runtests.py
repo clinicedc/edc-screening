8216abce5a95aaaa163010d58b813211a92d2131
erikvw <ew2789@gmail.com>
2019-06-15 14:48:13 -0500

    initial

---
+++ runtests.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import django
+import logging
+import os
+import sys
+
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+from os.path import abspath, dirname
+
+app_name = 'edc_screening'
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    template_dirs=[os.path.join(
+        base_dir, app_name, "tests", "templates")],
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    # SUBJECT_VISIT_MODEL="edc_screening.subjectvisit",
+    INSTALLED_APPS=[
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+        'django_extensions',
+        'simple_history',
+        # 'django_crypto_fields.apps.AppConfig',
+        # 'django_revision.apps.AppConfig',
+        #         'edc_consent.apps.AppConfig',
+        #         'edc_dashboard.apps.AppConfig',
+        'edc_device.apps.AppConfig',
+        #         'edc_lab.apps.AppConfig',
+        #         'edc_metadata_rules.apps.AppConfig',
+        #         'edc_navbar.apps.AppConfig',
+        #         'edc_offstudy.apps.AppConfig',
+        #         'edc_protocol.apps.AppConfig',
+        #         'edc_reference.apps.AppConfig',
+        #         'edc_registration.apps.AppConfig',
+        #         'edc_subject_dashboard.apps.AppConfig',
+        #         'edc_timepoint.apps.AppConfig',
+        #         'edc_visit_schedule.apps.AppConfig',
+        #         'edc_model_admin.apps.EdcAppointmentAppConfig',
+        #         'edc_model_admin.apps.EdcFacilityAppConfig',
+        #         'edc_model_admin.apps.EdcMetadataAppConfig',
+        #         'edc_model_admin.apps.EdcVisitTrackingAppConfig',
+        'edc_identifier.apps.AppConfig',
+        'edc_screening.apps.AppConfig',
+    ],
+    DASHBOARD_BASE_TEMPLATES={
+        'dashboard_template': os.path.join(
+            base_dir, 'edc_screening', "tests", 'templates', "dashboard.html"),
+        'dashboard2_template': os.path.join(
+            base_dir, 'edc_screening', "tests", 'templates', "dashboard2.html")},
+    use_test_urls=True,
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split('=')[1] for t in sys.argv if t.startswith('--tag')]
+    failures = DiscoverRunner(
+        failfast=False, tags=tags).run_tests([f'{app_name}.tests'])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
