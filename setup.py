faaf735e413dab46e492e00d6fbc05395eb60a53
erikvw <ew2789@gmail.com>
2020-03-06 09:39:33 -0600

    test setup

--- setup.py
+++ setup.py
@@ -4,52 +4,40 @@ from setuptools import setup
 from setuptools import find_packages
 from os.path import join, abspath, dirname, normpath
 
-with open(join(dirname(__file__), 'README.rst')) as readme:
+with open(join(dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
-with open(join(dirname(__file__), 'VERSION')) as f:
+with open(join(dirname(__file__), "VERSION")) as f:
     VERSION = f.read()
-
-tests_require = ["edc_test_utils"]
-with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
-    for line in f:
-        tests_require.append(line.strip())
 
 # allow setup.py to be run from any path
 os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
 setup(
-    name='edc_screening',
+    name="edc_screening",
     version=VERSION,
-    author=u'Erik van Widenfelt',
-    author_email='ew2789@gmail.com',
+    author=u"Erik van Widenfelt",
+    author_email="ew2789@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/clinicedc/edc_screening',
-    license='GPL license, see LICENSE',
-    description='Screening fields and method mixins.',
+    url="https://github.com/clinicedc/edc_screening",
+    license="GPL license, see LICENSE",
+    description="Screening fields and method mixins.",
     long_description=README,
     zip_safe=False,
-    keywords='django edc screening',
-    install_requires=[
-        'edc_model',
-        'edc_sites',
-        'edc_utils',
-        'edc_identifier',
-        'edc_search',
-    ],
+    keywords="django edc screening",
+    install_requires=[],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     python_requires=">=3.7",
-    tests_require=tests_require,
-    test_suite='runtests.main',
+    test_suite="runtests.main",
 )
