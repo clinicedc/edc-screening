f146a79b55fe9b73970b5e38f3155cc29823d297
erikvw <ew2789@gmail.com>
2022-08-24 09:45:27 -0500

    update permissions for edc_listboard, edc_dashboard

--- edc_screening/auths.py
+++ edc_screening/auths.py
@@ -9,19 +9,19 @@ from edc_auth.site_auths import site_auths
 from .auth_objects import SCREENING, SCREENING_ROLE, SCREENING_SUPER, SCREENING_VIEW
 
 site_auths.add_group(
-    "edc_dashboard.view_screening_listboard",
+    "edc_listboard.view_screening_listboard",
     "edc_navbar.nav_screening_section",
     name=SCREENING,
 )
 
 site_auths.add_group(
-    "edc_dashboard.view_screening_listboard",
+    "edc_listboard.view_screening_listboard",
     "edc_navbar.nav_screening_section",
     name=SCREENING_SUPER,
 )
 
 site_auths.add_group(
-    "edc_dashboard.view_screening_listboard",
+    "edc_listboard.view_screening_listboard",
     "edc_navbar.nav_screening_section",
     name=SCREENING_VIEW,
 )
