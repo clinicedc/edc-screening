fa81448811850e8c05bed7eb7d31aec71090db2d
erikvw <ew2789@gmail.com>
2023-07-04 19:41:58 -0500

    move managers from fields mixin to screening_model_mixin

--- edc_screening/model_mixins/screening_model_mixin.py
+++ edc_screening/model_mixins/screening_model_mixin.py
@@ -1,9 +1,19 @@
 from django.db import models
+from edc_model.models import HistoricalRecords
+from edc_search.model_mixins import SearchSlugManager
+from edc_sites.model_mixins import CurrentSiteManager
 
 from ..screening_identifier import ScreeningIdentifier
 from .screening_fields_model_mixin import ScreeningFieldsModeMixin
 from .screening_identifier_model_mixin import ScreeningIdentifierModelMixin
 from .screening_methods_model_mixin import ScreeningMethodsModeMixin
+
+
+class ScreeningManager(SearchSlugManager, models.Manager):
+    use_in_migrations = True
+
+    def get_by_natural_key(self, screening_identifier):
+        return self.get(screening_identifier=screening_identifier)
 
 
 class ScreeningModelMixin(
@@ -18,9 +28,15 @@ class ScreeningModelMixin(
 
     identifier_cls = ScreeningIdentifier
 
-    # add this to your concrete model if asking for site confirmation
-    # on the screening form to make the field editable.
+    # add `site` to your concrete model if asking for site confirmation
+    # and on the screening form to make the field editable.
     # site = models.ForeignKey(Site, on_delete=models.PROTECT, null=True, related_name="+")
+
+    objects = ScreeningManager()
+
+    on_site = CurrentSiteManager()
+
+    history = HistoricalRecords(inherit=True)
 
     class Meta:
         abstract = True
