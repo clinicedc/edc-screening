c073b98786ff711b33d67b75ea060d92fb7c8084
erikvw <ew2789@gmail.com>
2023-12-04 22:48:49 -0600

    review model meta class inheritance for indexes, locale

--- edc_screening/models.py
+++ edc_screening/models.py
@@ -5,5 +5,5 @@ from edc_model.models import BaseUuidModel
 class EdcPermissions(EdcPermissionsModelMixin, BaseUuidModel):
     # see edc_auth for permissions attached to this model
 
-    class Meta(EdcPermissionsModelMixin.Meta):
+    class Meta(EdcPermissionsModelMixin.Meta, BaseUuidModel.Meta):
         pass
